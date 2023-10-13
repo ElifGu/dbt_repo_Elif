17f42b3ad3945f804f7e7fb3471757f82d499ef8
ElifGu <gumuselif84@gmail.com>
2023-10-13 11:25:20 +0200

    prep_temp update3

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -1,16 +1,27 @@
+-- WITH temp_daily AS (
+--     SELECT * 
+--     FROM {{ref('staging_weather')}}
+-- ),
+-- add_weekday AS (
+--     SELECT *,
+--         date_part ('weekday', date)AS weekday,
+--         date_part('day', date) AS day_num
+--     FROM temp_daily
+-- )
+-- SELECT *
+-- FROM add_weekday
+
+
 WITH temp_daily AS (
     SELECT * 
     FROM {{ref('staging_weather')}}
 ),
 add_weekday AS (
     SELECT *,
-        date_part ('weekday', date)AS weekday,
+        date_part('dow', date) AS weekday,
         date_part('day', date) AS day_num
     FROM temp_daily
 )
 SELECT *
 FROM add_weekday
-
-
-
 
