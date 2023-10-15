54f2bd74df819b429497a25015bab47906dca8e9
ElifGu <gumuselif84@gmail.com>
2023-10-15 19:53:57 +0200

    3rd mart

---
+++ models/mart/monthly_min_temo_by_location.sql
@@ -0,0 +1,17 @@
+with prep_temp_data as (
+    SELECT * 
+    FROM {{ref('prep_temp')}}
+),
+monthly_min as(
+    SELECT date_part('month', date) AS month,
+    min(mintemp_c) AS min_temp,
+    city
+    from prep_temp_data
+    WHERE date is not null
+    GROUP BY month, city
+    ORDER BY month, min_temp
+)
+select *
+FROM monthly_min
+
+
