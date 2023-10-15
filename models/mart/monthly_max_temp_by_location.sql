b48373ab3b47e22f3e9857d1577c4f7c3e91e7bb
ElifGu <gumuselif84@gmail.com>
2023-10-15 19:59:16 +0200

    4th mart

---
+++ models/mart/monthly_max_temp_by_location.sql
@@ -0,0 +1,15 @@
+with prep_temp_data as (
+    SELECT * 
+    FROM {{ref('prep_temp')}}
+),
+monthly_min as(
+    SELECT date_part('month', date) AS month,
+    max(maxtemp_c) AS max_temp,
+    city
+    from prep_temp_data
+    WHERE date is not null
+    GROUP BY month, city
+    ORDER BY month, max_temp
+)
+select *
+FROM monthly_min
