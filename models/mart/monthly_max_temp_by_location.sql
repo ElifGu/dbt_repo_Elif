0ad9e51346648ff1ad67ef228160535ad4fe8f18
ElifGu <gumuselif84@gmail.com>
2023-10-20 09:21:26 +0200

    test commit lat and lon

--- models/mart/monthly_max_temp_by_location.sql
+++ models/mart/monthly_max_temp_by_location.sql
@@ -2,15 +2,17 @@ with prep_temp_data as (
     SELECT * 
     FROM {{ref('prep_temp')}}
 ),
-monthly_min as(
+monthly_max as(
     SELECT date_part('month', date) AS month,
     max(maxtemp_c) AS max_temp,
     city,
-    country
+    country,
+    lat,
+    lon
     from prep_temp_data
     WHERE date is not null
     GROUP BY month, city, country
     ORDER BY month, max_temp
 )
 select *
-FROM monthly_min
+FROM monthly_max
